49f3cb1da3fd513a560343af10fe4a03b0253784
daudfauzy98 <daudfauzy98@gmail.com>
2020-11-18 14:28:28 +0700

    Update Ticket model attribute

--- controller/TicketController.js
+++ controller/TicketController.js
@@ -3,8 +3,10 @@ import Ticket from '../model/ticket.js'
 
 const ticketRouter = express.Router()
 
-ticketRouter.post('/', async(req, res) => {
+// Create ticket by Customers
+ticketRouter.post('/init_ticket', async(req, res) => {
    // Insert logic here
+   
 })
 
 ticketRouter.get('/', async(req, res) => {
