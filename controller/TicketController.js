b274d48b091469d0fbd15e908390d2daad209e91
b-rachman <71545716+b-rachman@users.noreply.github.com>
2020-11-19 12:23:31 +0700

    add Endpoint for search ticket by tag and category

--- controller/TicketController.js
+++ controller/TicketController.js
@@ -24,4 +24,23 @@ ticketRouter.get('/ticketList/unread', async(req, res) => {
 
 })
 
+//GET Ticket search based on tag and category endpoint
+ticketRouter.get('/ticketlist/search',async(req,res)=>{
+    const tickets = await Ticket.aggregate(
+        [
+            {
+                $match:{tag:String(req.query.tag)}
+            },
+            {
+                $match:{category:String(req.query.category)}
+            }
+        ]
+    )
+    if(tickets){
+        res.json(tickets)
+    } else{
+        res.send("Ticket not found")
+    }
+})
+
 export default ticketRouter
