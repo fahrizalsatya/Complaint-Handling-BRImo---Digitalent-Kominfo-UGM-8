6e217c32ad664bbb5bca50d2e2bb89788553d9b2
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-19 15:17:54 +0700

    Fix Resend Email Verification

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -2,9 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import Customer from '../model/customer.js'
 import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
 import Config from '../config/config.js'
 import secretCode from '../model/secretCode.js'
+import nodemailer from 'nodemailer'
 
 const CustomerRouter = express.Router()
 
@@ -16,9 +18,9 @@ CustomerRouter.use(bodyParser.json())
 CustomerRouter.post('/signup', async(req, res) => {
     try {
         const { name, email, password, account_number, no_ktp } = req.body
-        Customer.findOne({ email }, function(err, customer) {
+        Customer.findOne({ $or: [{ email }, { account_number }, { no_ktp }] }, async(err, customer) => {
             if (customer) {
-                res.status(201).json({ message: 'The email address you have entered is already associated with another account.' })
+                res.status(201).json({ message: 'The email address or identity you have entered is already associated with another account.' })
             } else {
                 var saltRounds = 12
                 const hashedPassword = await bcrypt.hash(password, saltRounds)
@@ -45,7 +47,7 @@ CustomerRouter.post('/signup', async(req, res) => {
 
                         // Send the email
                         var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: process.env.MAIL, pass: process.env.PASS } });
-                        var mailOptions = { from: process.env.MAIL, to: createdCust.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/verify\/' + customer.email + '\/' + token.token + '.\n' };
+                        var mailOptions = { from: process.env.MAIL, to: customer.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/verify\/' + customer.email + '\/' + token.token + '.\n' };
                         transporter.sendMail(mailOptions, function(err) {
                             if (err) { return res.status(500).send({ msg: err.message }); }
                             res.status(200).send('A verification email has been sent to ' + customer.email + '.');
@@ -60,7 +62,7 @@ CustomerRouter.post('/signup', async(req, res) => {
 })
 
 //SEND MAIL
-CustomerRouter.get('/send', async(req, res) => {
+CustomerRouter.post('/send', async(req, res) => {
     Customer.findOne({ email: req.body.email }, function(err, customer) {
         if (!customer) return res.status(400).send({ msg: 'We were unable to find a user with that email.' });
         if (customer.isVerified) return res.status(400).send({ msg: 'This account has already been verified. Please log in.' });
@@ -86,7 +88,7 @@ CustomerRouter.get('/send', async(req, res) => {
 
 //Verify
 //GET /api/customer/verify
-CustomerRouter.get('/verify/:email/:token', async(req, res) => {
+CustomerRouter.post('/verify/:email/:token', async(req, res) => {
     // Find a matching token
     secretCode.findOne({ token: req.params.token }, function(err, token) {
         if (!token) return res.status(400).send({ type: 'not-verified', msg: 'We were unable to find a valid token. Your token my have expired.' });
