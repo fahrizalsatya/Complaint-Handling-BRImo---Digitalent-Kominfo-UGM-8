7446355392017353638c402f938c1186cf81c05e
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-20 02:21:58 +0700

    Edit Schema

--- controller/SpvController.js
+++ controller/SpvController.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import Supervisor from '../model/spv.js'
+import Supervisor from '../model/cs.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import Config from '../config/config.js'
@@ -11,6 +12,7 @@ SpvRouter.use(bodyParser.urlencoded({ extended: false }))
 SpvRouter.use(bodyParser.json())
 
 //Login SPV
+//POST /api/spv/login
 SpvRouter.post('/login', async(req, res) => {
     try {
         const { email, password } = req.body
@@ -45,35 +47,38 @@ SpvRouter.post('/login', async(req, res) => {
     }
 })
 
-//GET Escalated Ticket List for SPV
-SpvRouter.get('/ticket-list/escalated', async(req, res) => {
-    const tickets = await Ticket.aggregate({
-        $match: { tag: 'ESCALATED' }
-    })
-    if (tickets) {
-        res.json(tickets)
-    } else {
-        res.send("Ticket not found")
+//Mendaftarkan CS
+//POST /api/spv/add-cs
+spvRouter.post('/add-cs', async(req, res) => {
+    try {
+        const { name, personal_id, email, password, photo, pub_name, pub_photo, final_rating } = req.body
+        const csAccount = await CustService.findOne({ email })
+
+        if (csAccount) {
+            res.status(201).json({
+                message: 'Email telah terdaftar'
+            })
+        } else {
+            var saltRounds = 12
+            const hashedPw = await bcrypt.hash(password, saltRounds)
+            const createdCS = new CustService({
+                "name": name,
+                "personal_id": personal_id,
+                "email": email,
+                "password": hashedPw,
+                "photo": photo,
+                "pub_name": pub_name,
+                "pub_photo": pub_photo,
+                "final_rating": final_rating
+            })
+            const savedCS = await createdCS.save()
+            res.status(201).json(savedCS)
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        })
     }
 })
-
-//GET Escalated Ticket List by Category for SPV
-SpvRouter.get('/ticket-list/escalated/:category', async(req, res) => {
-    const tickets = await Ticket.aggregate(
-        [{
-                $match: { tag: 'ESCALATED' }
-            },
-            {
-                $match: { category: String(req.params.category) }
-            }
-        ]
-    )
-    if (tickets) {
-        res.json(tickets)
-    } else {
-        res.send("Ticket not found")
-    }
-})
-
 
 export default SpvRouter
